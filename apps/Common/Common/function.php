82849eaa6738a96453973697e5348eaa4cf14727
许小平 <xuxiaoping@wordemotion.com>
2016-09-10 14:47:07 +0800

    完成组织申请页面

---
+++ apps/Common/Common/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiao
+ * Date: 16/9/10
+ * Time: 下午1:36
+ */
---
+++ apps/Home/Controller/OrigniseController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class BaseController extends Controller {
+
+    public function msg(){
+        
+    }
+        
+
+    public function index1($value='')
+    {
+        # code...
+
+    }
+}
